95052245b6bfbdbb203374e6adb6e41ab20dafed
Joseph <129715233+Jooi025@users.noreply.github.com>
2024-03-02 03:20:56 +1100

    added python file

---
+++ src/flask-server/main.py
@@ -0,0 +1,53 @@
+from ics import Calendar
+import requests
+from datetime import datetime,date,timedelta
+
+DISPLAY_WEEK = 2
+START_TIME = 8 #8AM
+END_TIME = 20 #8PM
+
+#iso format: YYYY-MM-DD"T"hh:mm:ss"+"{UTC-zone}
+def get_date(isoTime):
+    dateList = isoTime.split("T")[0].split("-")
+    return [int(dates) for dates in dateList]
+
+def get_time(isoTime):
+    utcTime = isoTime.split("T")[1]
+    return utcTime.split("+")[0]
+
+def main():
+    lastestDate = None
+    return_dict = {}
+    # link from allocate+
+    url = "https://my-timetable.monash.edu/even/rest/calendar/ical/a1df181d-05a3-4719-b029-bddb5f79e676"
+    cal = Calendar(requests.get(url).text)
+    currentDate = date.today()
+    boundaryDate = currentDate + timedelta(days=7*DISPLAY_WEEK)
+
+    for event in list(cal.timeline):
+        eventBeginDateList = get_date(str(event.begin))
+
+        eventBeginDate = date(eventBeginDateList[0],
+                            eventBeginDateList[1],
+                            eventBeginDateList[2])
+        # ignore the past dates
+        if eventBeginDate < currentDate:
+            continue
+        # also ignore the dates more the set week
+        if eventBeginDate > boundaryDate:
+            break
+
+        return_dict[str(eventBeginDate)] =[str(event.duration)]
+        return return_dict
+        # if not(lastestDate is None) and lastestDate == str(eventBeginDate):
+        #     return_dict[str(eventBeginDate)] = 
+        # lastestDate = str(eventBeginDate)
+
+        # print(event.name)
+        # print(event.location)
+        # print(event.duration)
+        # print(get_time(str(event.begin))) 
+        # print(eventBeginDate)
+
+
+print(main())
--- src/flask-server/server.py
+++ src/flask-server/server.py
@@ -1,10 +1,10 @@
 from flask import Flask
-
+from main import main
 app = Flask(__name__)
 
 @app.route("/members")
 def members():
-    return {"members": ["member1", "member2"]}
+    return main()
 
 if __name__ == "__main__":
     app.run(debug=True)
