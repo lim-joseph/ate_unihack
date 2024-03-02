e0e2eb027e31c99de668802ba6565fa5ddb4bed8
Joseph <129715233+Jooi025@users.noreply.github.com>
2024-03-03 01:59:33 +1100

    added database

--- src/flask-server/app.py
+++ src/flask-server/app.py
@@ -1,8 +1,17 @@
 import sys
-from flask import Flask
+from flask import Flask,request
 from flask import __version__ as flask_version
 from main import main
+import os
+from supabase import create_client
+from flask_cors import CORS
+import json
+from tok import url,key
+
+supabase = create_client(url, key)
+
 app = Flask(__name__)
+CORS(app)
 
 @app.route("/")
 def hello():
@@ -14,6 +23,29 @@ def hello():
 def data():
     ics_url = request.args.get('ics-url')
     return main(ics_url)
+
+@app.route("/user/<username>")
+def get_user(username):
+    res= supabase.table('student').select("*").eq("username",username).execute().data
+    print(res)
+    return res
+
+@app.route("/create",methods=['POST'])
+def create_user():
+    data = request.data
+    data=json.loads(str(data, encoding='utf-8'))
+    supabase.table('student').insert(data).execute()
+    return "success"
+
+@app.route("/freetime/<username>")
+def get_free_time_route(username):
+    res = supabase.table('student').select("*").eq("username",username).execute().data
+    if(res):
+        timetable_link = res[0]['timetable_link']
+    else:
+        return 'No user', 400
+    return main(timetable_link)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
