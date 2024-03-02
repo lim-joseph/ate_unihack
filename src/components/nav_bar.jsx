88a72b2a01266e507a386cd5c73f4ff08ac14114
applewendy12 <wendy.huangxi87@outlook.com>
2024-03-02 11:26:25 +1100

    nav_bar

--- src/components/nav_bar.jsx
+++ src/components/nav_bar.jsx
@@ -14,19 +14,15 @@ export default function NavBar() {
           </Link>
         </NavbarItem>
         <NavbarItem isActive>
-          <Link href="#" aria-current="page">
-            Customers
-          </Link>
+  
         </NavbarItem>
         <NavbarItem>
-          <Link color="foreground" href="#">
-            Integrations
-          </Link>
+  
         </NavbarItem>
       </NavbarContent>
       <NavbarContent justify="end">
         <NavbarItem className="hidden lg:flex">
-          <Link href="#">Login</Link>
+
         </NavbarItem>
         <NavbarItem>
           <Button as={Link} color="primary" href="#" variant="flat">
