6d37e49100481d2211ea0ae61f0f48cc6cf059ee
Toby56 <Toby56@pm.me>
2024-03-02 23:45:06 +1100

    Add flask server hello message

--- src/flask-server/app.py
+++ src/flask-server/app.py
@@ -1,10 +1,15 @@
+import sys
 from flask import Flask
+from flask import __version__ as flask_version
 from main import main
 app = Flask(__name__)
 
-@app.route("/members")
-def members():
-    return main()
+@app.route("/")
+def hello():
+    return (f"""Allodate+ backend :)<br>
+            Python {sys.version}<br>
+            Flask {flask_version}""")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
